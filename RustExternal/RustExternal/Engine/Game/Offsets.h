0ea8ad7cb298870e87144196f4c5b0dd82a04586
ilyes009 <ilyesseddi8@gmail.com>
2025-02-19 02:16:54 +0100

    Ajoutez des fichiers projet.

---
+++ RustExternal/RustExternal/Engine/Game/Offsets.h
@@ -0,0 +1,14 @@
+#pragma once
+#ifndef __OFFSETS_H__
+#define __OFFSETS_H__
+
+#include <stdint.h>
+
+namespace IOffset
+{
+	inline uintptr_t dwLocalPlayer = 62090880;			// LocalPlayer_TypeInfo
+	inline uintptr_t dwBaseNetworkable = 62187376;		// BaseNetworkable_TypeInfo
+	inline uintptr_t dwCameraManager = 62102696;		// MainCamera_TypeInfo
+}
+
+#endif
